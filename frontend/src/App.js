461483fc704ef13f8c2d6be8529aae9096ba3bd0
Piotr Kocik <lifesim11@gmail.com>
2021-03-24 14:10:23 +0100

    added header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <main className='py-3'>
+      <h1>Welcome to the store</h1>
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">E-commerce store</Navbar.Brand>
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
