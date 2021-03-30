8725b0da7ab28cd4afb1031b4bff5078cf9be669
Piotr Kocik <lifesim11@gmail.com>
2021-03-30 22:25:42 +0200

    added loader and message components

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,7 +3,7 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-} from "..constants/productConstants";
+} from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
     try {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from 'axios';
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+  //to dispatch listProducts action
+  //using hooks
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products');
-            setProducts(data);
-        }
+  const productList = useSelector((state) => state.productList);
+  //part of the state that could be sent down
+  const { loading, error, products } = productList;
 
-        fetchProducts();
-    }, []);
+
+
+  //makes a request to backend to request products
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  const products = [];
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} x={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader/>
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} x={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
