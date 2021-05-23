f42448fe029120c7bd534d5f28fa95c254c87184
Piotr Kocik <lifesim11@gmail.com>
2021-05-23 15:29:51 +0200

    added product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,12 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
+import express from "express";
 const router = express.Router();
-import Product from '../models/productModel.js';
-
-//fetch all products
-//GET /api/products
-router.get('/', async (req, res) => {
-const products = await Product.find({});
-
-    res.json(products);
-});
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 
-//fetch a single product
-//GET /api/products/:id
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if(product){
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
+
+
+
 
 export default router;
