615a9124fc96485ae4999e99018cab9fbea7ef1b
Piotr Kocik <lifesim11@gmail.com>
2021-05-26 21:03:10 +0200

    added user card to navbar and logout funtionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,46 @@
 import React from "react";
-import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import {logout} from "../actions/userActions";
+//when we want bring anything from redux state, we need to use useDispatch (calling an action) and useSelector (bring something in/state)
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const logoutHandler = () => {
+   dispatch(logout);
+  }
+
   return (
     <header>
       <Navbar bg="light" expand="lg">
-      <Container>
-        <LinkContainer to='/'>
-        <Navbar.Brand>E-commerce store</Navbar.Brand>
-        </LinkContainer>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>E-commerce store</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-            <LinkContainer to='/cart'>
-              <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-              <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name}>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Profile</NavDropdown.Item>
+                </NavDropdown>
+              ) : <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </LinkContainer>}
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
