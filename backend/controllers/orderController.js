9451fe2c2c58c96dcee8c08668f3822235c00e96
Piotr Kocik <lifesim11@gmail.com>
2021-06-03 13:25:38 +0200

    added api endpoint for getting an order by ID

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,23 +13,33 @@ const addOrderItems = asyncHandler(async (req, res) => {
     shippingPrice,
   } = req.body;
 
-  if(orderItems && orderItems.length === 0){
-      res.status(400);
-      throw new Error('No order items');
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
   } else {
-      const order = new order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        textPrice,
-        shippingPrice,
-      });
+    const order = new order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      textPrice,
+      shippingPrice,
+    });
 
-      const createdOrder = await order.save();
-      res.status(201).json(createdOrder);
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
   }
 });
 
-export { addOrderItems};
+//get order by id
+//GET api/orders/:id
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await (await Order.findById(req.params.id)).populated("user", "name", "email");
+  if(order) {
+    res.json(order);
+  } else {
+    throw new Error("Order not found.");
+  }
+});
+export { addOrderItems, getOrderById };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 const router = express.Router();
 import {
   addOrderItems,
+  getOrderById
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 //route for user registration
 router.route("/").post(protect, addOrderItems);
+
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
